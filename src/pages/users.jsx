e9e5587023dbadc25fb07ef9ca08e8849550a9d8
LaySatya <141922646+LaySatya@users.noreply.github.com>
2025-04-05 05:06:20 +0700

    filter-user-by-role-name

--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { User, Users2 } from "lucide-react";
 import { useEffect, useState } from "react";
 
 const Users = () => {
@@ -7,6 +8,8 @@ const Users = () => {
     const [error, setError] = useState(null);
     const [searchTerm, setSearchTerm] = useState("");
     const [statusFilter, setStatusFilter] = useState("");
+    const [roleFilter, setRoleFilter] = useState("");
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -40,11 +43,13 @@ const Users = () => {
         fetchData();
     }, []);
 
-    const filteredUsers = users.filter(user => 
+    const filteredUsers = users.filter(user =>
         (user.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        user.email.toLowerCase().includes(searchTerm.toLowerCase())) &&
-        (statusFilter ? user.status === statusFilter : true)
+            user.email.toLowerCase().includes(searchTerm.toLowerCase())) &&
+        (statusFilter ? user.status === statusFilter : true) &&
+        (roleFilter ? user.role === roleFilter : true)
     );
+
 
     if (loading) return <div className="flex justify-center items-center">Loading...</div>;
     if (error) return <p>Error: {error}</p>;
@@ -52,17 +57,28 @@ const Users = () => {
     return (
         <>
             <div className="flex justify-between items-center">
-                <h2 className="text-xl font-bold">📦 Users</h2>
+                <h2 className="text-xl font-bold flex pr-1"><Users2 className="m-0.5 mr-1" /> Users</h2>
                 <div className="flex ">
                     <label htmlFor="" className="m-2">Filter:</label>
                     <input
                         type="text"
                         placeholder="Search by name or email"
-                        className="input input-bordered w-56 mr-2"
+                        className="input input-bordered w-56"
                         value={searchTerm}
                         onChange={(e) => setSearchTerm(e.target.value)}
                     />
-                    <select 
+                    <select
+                        className="select select-bordered w-28 border-sky-300 mx-2"
+                        value={roleFilter}
+                        onChange={(e) => setRoleFilter(e.target.value)}
+                    >
+                        <option value="">All Roles</option>
+                        <option value="Admin">Admin</option>
+                        <option value="Customer">Customer</option>
+                        <option value="Driver">Driver</option>
+                    </select>
+
+                    <select
                         className="select select-bordered w-28 border-amber-300"
                         value={statusFilter}
                         onChange={(e) => setStatusFilter(e.target.value)}
@@ -75,7 +91,7 @@ const Users = () => {
             </div>
 
             <div className="mt-5">
-                <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+                <div className="overflow-x-auto h-96 rounded-box border border-base-content/5 bg-base-100">
                     <table className="table">
                         <thead>
                             <tr>
@@ -100,7 +116,7 @@ const Users = () => {
                                         <td>{user.email}</td>
                                         <td>{user.phone}</td>
                                         <td>
-                                            <span className={`badge ${user.status === "active" ? "badge-success" : "badge-error"}`}>{user.status}</span>
+                                            <span className={`badge ${user.status === "active" ? "badge-success badge-soft" : "badge-error badge-soft"}`}>{user.status}</span>
                                         </td>
                                         <td>{user.role}</td>
                                     </tr>
